f392740b6c915994954484329e599ba707934cbe
RonakAgarwal <agarwalronak02@gmail.com>
2019-07-22 14:00:35 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,22 @@
+class Artist
+
+  attr_accessor :name
+
+  def initialize(name)
+
+    @name = name
+    @songs = []
+
+  end
+
+  def add_song(song)
+
+    @songs << song
+    song.artist = self
+    @@song_count +=1
+
+  end
+
+  def add_song_by_name (name)
+
+    
